5696831b703e8fe9c333d4429405fec975176130
Adriano Interaminense <adriano.interaminense@liferay.com>
2017-09-29 17:04:17 -0300

    update add and remove functions

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,16 +7,57 @@ import Soy from 'metal-soy';
 import './todo-list.scss';
 
 class TodoList extends Component {
-    newElement(event) {
-        this.setState({task: this.task.concat(event.target.value)});
-    }
+	newTask(event) {
+		let newTask = {
+			description: event.target.value,
+			done: false
+		}
 
+		this.setState({tasks: this.tasks.concat(newTask)});
+
+		event.target.value = "";
+	}
+
+	handleCheckTask_(event) {
+		let index = event.delegateTarget.getAttribute("data-index");
+		let tempTask = {
+			description: this.tasks[index].description,
+			done: !this.tasks[index].done
+		}
+
+		this.tasks.splice(index, 1, tempTask);
+		this.setState({tasks: this.tasks});
+	}
+
+	handleRemoveTask_(event) {
+		let index = event.target.getAttribute("data-index");
+
+		this.tasks.splice(index, 1);
+		this.setState({tasks: this.tasks});
+	}
 }
 
+
 TodoList.STATE = {
-    task: {
-        value: []
-    }
+	tasks: {
+		value: [
+			{
+				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer auctor sollicitudin enim vitae imperdiet. Morbi et faucibus purus, at gravida justo. Proin in nisi molestie, consectetur mi a, eleifend augue. Sed mollis libero at fermentum hendrerit.',
+				done: false
+			},
+			{
+				description: 'Cras pharetra sodales lorem, ac euismod felis consectetur ac. Pellentesque tempus sapien mauris, ut pharetra lacus porta in.',
+				done: true
+			},
+			{
+				description: 'Ut diam enim, mollis id facilisis vel, pulvinar interdum elit.',
+				done: false
+			}
+		]
+	},
+	removedTasks: {
+		value: []
+	}
 }
 
 Soy.register(TodoList, templates);
