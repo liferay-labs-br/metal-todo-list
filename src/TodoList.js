bc7c0d3c80ebdaa334c34fc46e99f663615df685
Adriano Interaminense <adriano.interaminense@liferay.com>
2017-10-07 11:51:58 -0300

    Add Component Progress Bar

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,6 +4,7 @@ import templates from './TodoList.soy.js';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
 import WeDeploy from 'wedeploy';
+import './components/ProgressBar';
 
 import './todo-list.scss';
 
